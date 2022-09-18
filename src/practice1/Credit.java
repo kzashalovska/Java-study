1c30d490d4256200ef1dc840b182c73353649607
Yevdokimov Denys <104136673+Yevdokimov@users.noreply.github.com>
2022-09-18 13:31:25 +0300

    lesson 3 - add class, which calculate sum of credit pays.

--- src/practice1/Credit.java
+++ src/practice1/Credit.java
@@ -9,55 +9,48 @@ public class Credit {
     public static void main(String[] args) {
 
         // КЛАСИЧНИЙ МЕТОД РОЗРАХУНКУ
-        int period = 24; // період кредитування
-        float percent = 32.5F; // процентна ставка
-        float credit = 150000.00F; //сума кредиту
-        float bodypay = credit/period; // Змінна для зберігання щомісяного платежу по тілу кредиту
-        float allpercent = 0; // Змінна для підрахунку загальної суми відсотків
+        int period = 24;
+        float interest = 32.5F;
+        final float mounthlyPay = interest / 100 / 12;
+        float creditSum = 150000.00F;
+        float bodyPay = creditSum / period;
+        float allInterestPay = 0;
 
         // Цикл розраховує розмір щомісячного платежу за тілом і відсотки
-        for ( int i = 1; i <= period; i++) {
-            // Вивід на екран інформації про сплачену суму тіла кредиту за місяць
-            System.out.println("Сума оплати тіла кредиту за місяць " + i + ": " + (String.format(Locale.ENGLISH, "%(.2f", bodypay)));
-            float percentpay = credit * percent/100/12; // Сума сплати відсотків у місяць
-            // Вивід на екран щомісячної суми сплачених відсотків
-            System.out.println("Сума оплати відсотків за місяць " + i + ": " + (String.format(Locale.ENGLISH, "%(.2f", percentpay)));
-            credit = credit - bodypay; // Зміна розміру тіла кредиту після сплати ануїтетного платежу
-            allpercent = allpercent + percentpay; // Підрахунок загальної суми сплачених відсотків
+        for (int i = 1; i <= period; i++) {
+            System.out.println("Сума оплати тіла кредиту за місяць " + i + ": " + getFormat(bodyPay));
+            float percentpay = creditSum * mounthlyPay; // Сума сплати відсотків у місяць
+            System.out.println("Сума оплати відсотків за місяць " + i + ": " + getFormat(percentpay));
+            creditSum = creditSum - bodyPay; // Зміна розміру тіла кредиту після сплати ануїтетного платежу
+            allInterestPay = allInterestPay + percentpay; // Підрахунок загальної суми сплачених відсотків
         }
         // Вивід на екран загальної суми сплачених відсотків
-        System.out.println("Загальна сума відсотків: " + (String.format(Locale.ENGLISH, "%(.2f", allpercent)));
+        System.out.println("Загальна сума відсотків: " + getFormat(allInterestPay));
 
         //АНУЇТЕТНИЙ МЕТОД РОЗРАХУНКУ
+
         // Розрахунок ануїтетного коефіцієнту
-        double k = (percent/100/12 * pow((1+percent/100/12), period)) / (pow((1 + percent/100/12), period) - 1);
-        float credit2 = 150000.00F; // Сума кредиту для другого варіанта
-        float monthpay = (float) (credit2 * k); // Розмір щомісяного платежу
-        float sumcredit = period * monthpay; // Загальна сума кредиту
-        float bodypay2; // Змінна для зберігання щомісяного платежу по тілу кредиту
-        float allpercent2 = 0; // Змінна для підрахунку загальної суми відсотків
-        // Цикл підраховучий щомісячні платежі по тілу та відсоткам
-        for (int i = 1; i <= period; i++){
-            float percentpay = credit2 * percent/100/12; // Сума сплати відсотків у місяць
-            bodypay2 = monthpay - percentpay; // Сума сплати тіла кредиту в місяць
-            // Вивід на екран інформації про сплачену суму тіла кредиту за місяць
-            System.out.println("Сума оплати тіла кредиту за місяць " + i + ": " + (String.format(Locale.ENGLISH, "%(.2f", bodypay2)));
-            credit2 = credit2 - bodypay2; // Зміна розміру тіла кредиту після сплати ануїтетного платежу
-            allpercent2 = allpercent2 + percentpay; // Підрахунок загальної суми сплачених відсотків
-            // Вивід на екран щомісячної суми сплачених відсотків
-            System.out.println("Сума оплати відсотків за місяць " + i + ": " + (String.format(Locale.ENGLISH, "%(.2f", percentpay)));
+        double annuityСoefficient = (mounthlyPay * getPow(period, mounthlyPay)) / (getPow(period, mounthlyPay) - 1);
+        float creditSum2 = 150000.00F;
+        float monthPay = (float) (creditSum2 * annuityСoefficient);
+        float bodyPay2;
+        float allPercent2 = 0;
+        for (int i = 1; i <= period; i++) {
+            float percentPay = creditSum2 * mounthlyPay;
+            bodyPay2 = monthPay - percentPay;
+            System.out.println("Сума оплати тіла кредиту за місяць " + i + ": " + getFormat(bodyPay2));
+            creditSum2 = creditSum2 - bodyPay2;
+            allPercent2 = allPercent2 + percentPay;
+            System.out.println("Сума оплати відсотків за місяць " + i + ": " + getFormat(percentPay));
         }
-        // Вивід на екран загальної суми сплачених відсотків
-        System.out.println("Загальна сума відсотків: " + (String.format(Locale.ENGLISH, "%(.2f", allpercent2)));
-
-
-
-
-
-
-
+        System.out.println("Загальна сума відсотків: " + getFormat(allPercent2));
     }
 
+    private static double getPow(int period, float mounthlyPay) {
+        return pow((1 + mounthlyPay), period);
+    }
 
-
+    private static String getFormat(float amount) {
+        return String.format(Locale.ENGLISH, "%(.2f", amount);
+    }
 }
---
+++ src/practice1/ForEach.java
@@ -0,0 +1,8 @@
+package practice1;
+
+public class ForEach {
+    public static void main(String[] args) {
+        int nums [] = {1, 2, 3, 4, 5};
+
+    }
+}
